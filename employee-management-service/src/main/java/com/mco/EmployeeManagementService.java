bde26c3b79d05b6ff9898f9a61b4e35e64919cf6
Nikhil-Wadatkar-Repo <nikhil.wadatkar4693@gmail.com>
2026-06-18 02:50:42 +0530

    added

--- employee-management-service/src/main/java/com/mco/EmployeeManagementService.java
+++ employee-management-service/src/main/java/com/mco/EmployeeManagementService.java
@@ -2,10 +2,12 @@ package com.mco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 //@EnableCaching
+@EnableAsync
 @EnableTransactionManagement
 public class EmployeeManagementService {
 	
--- my-app-ui/src/App.js
+++ my-app-ui/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import EmployeesPage from "./pages/EmployeesPage";
+
 import "./App.css";
+import EmployeeAgGrid from "./pages/EmployeeAgGrid";
+import AgGridPage1 from "./pages/AgGridPage1";
 
 function HomePage() {
   return (
-    <div className="HomePage">
+    <div>
       <h1>Welcome</h1>
       <p>Use the navigation above to view all employees.</p>
     </div>
@@ -15,18 +17,20 @@ function HomePage() {
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <nav className="App-nav">
+      <div>
+        <header>
+          <nav>
             <Link to="/">Home</Link>
             <Link to="/employees">Employees</Link>
           </nav>
         </header>
 
-        <main className="App-main">
+        <main>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/employees" element={<EmployeesPage />} />
+
+            <Route path="/employees" element={<AgGridPage1 />} />
+            <Route path="/grid1" element={<EmployeeAgGrid />} />
           </Routes>
         </main>
       </div>
